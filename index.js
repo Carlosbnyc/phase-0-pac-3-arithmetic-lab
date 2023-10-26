00037e648b785a30ed5e32ba24daf86a4435a861
Carlos <bowyercarlos@gmail.com>
2023-10-26 18:53:16 -0400

    arithmetic functions added

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a = 955 ,b = 123){
+  return a + b;
+}
+
+function subtract(a = 500, b = 196){
+ return a - b;
+}
+
+function multiply(a = 36478.5, b = 2){
+ return a * b;
+}
+
+function divide (a = 3.283898305084746, b = 100){
+  return a / b;
+}
+
+function increment(a=0) {
+return a+= 1;
+}
+
+function decrement (a=5){
+  return a-= 1;
+}
+
+function makeInt (n= 674){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal (n= 1.1234) {
+  return parseFloat(n, 10);
+}
